b6b976018741c4668514b86a2400ed9ca23914c9
Vivek Anand <vivekanand1101@gmail.com>
2016-02-26 16:19:26 +0530

    codechef: prime palindrome(TLE)

---
+++ python/codechef/primepalindrome.py
@@ -0,0 +1,44 @@
+def main():
+    n = int(raw_input())
+
+    #because 2 shouldn't be caught in
+    #next if condition.
+    if n == 2:
+        return 2
+    #to ensure to check only odd
+    #numbers
+    elif n % 2 == 0:
+        n += 1
+
+    while True:
+        if is_prime(n) and is_palindrome(n):
+            print n
+            break
+        else:
+            #since every n entered here
+            #is odd, they should remain odd
+            n += 2
+
+def is_prime(n):
+    # 1 is neither prime nor
+    # nor composite
+    if n == 1:
+        return False
+    elif n == 2:
+        return True
+
+    for i in range(2, n//2):
+        if n % i == 0:
+            return False
+
+    return True
+
+def is_palindrome(n):
+    l = list(str(n))
+    for i in range(int(len(l)// 2)):
+        if l[i] != l[-1 - i]:
+            return False
+    return True
+
+if __name__ == '__main__':
+    main()
