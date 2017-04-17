5196eec1045b3c0c781816e1cd0dd2aaa3c2b8da
Vivek Anand <vivekanand1101@gmail.com>
2017-04-18 00:39:01 +0530

    Edit distance dp
    Signed-off-by: Vivek Anand <vivekanand1101@gmail.com>

--- python/dp/edit_distance.py
+++ python/dp/edit_distance.py
@@ -24,4 +24,25 @@ def edit_distance_recur(str1, str2):
     return min(insert_cost, remove_cost, replace_cost)
 
 
+def edit_distance(str1, str2):
+    ''' Returns the edit distance of the two strings '''
+
+    m = len(str1)
+    n = len(str2)
+
+    E = [[0 for x in range(n+1)] for i in range(m+1)]
+
+    for i in range(m+1):
+        for j in range(n+1):
+            if 0 in [i, j]:
+                E[i][j] = max(i, j)
+            elif str1[i-1] == str2[j-1]:
+                E[i][j] = E[i-1][j-1]
+            else:
+                E[i][j] = 1 + min(
+                    E[i][j-1], E[i-1][j], E[i-1][j-1])
+    return E[m][n]
+
+
 print edit_distance_recur(str1, str2)
+print edit_distance(str1, str2)
